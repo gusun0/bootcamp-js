8e3692eb666189151bf02a4140526bf718be502c
gusun0 <fpatan@mail.ru>
2020-10-11 21:08:37 -0500

    bootcamp js

---
+++ babel/dev/js/script.js
@@ -0,0 +1,13 @@
+//npm install @babel/cli @babel/core @babel/polyfill @babel/preset-env @babel/register gulp gulp-babel gulp-concat gulp-plumber gulp-uglify --save-dev
+
+
+const number = [1,2,3,4];
+
+const printNumbers = () => {
+	console.log(...numbers);
+}
+
+
+
+
+
